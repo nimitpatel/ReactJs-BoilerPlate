8038716dace396cc9e4a1e25a23d3c266cec638e
nimit patel <nimit2997@gmail.com>
2020-02-25 20:34:32 +0530

    ready to ship �

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+    return (
+        <>
+            <h1>Congratulation it's Working 🔥</h1>
+        </>
+    )
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const htmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /public\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|jpeg)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebPackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
+};
